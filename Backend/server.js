4cf0c635c3878c8d02b4038fba618d563a7fa90c
vishalsuriya <vishalsuriya2003@gmail.com>
2025-07-18 09:56:00 +0530

    Implement user and history routes in the backend

--- Backend/server.js
+++ Backend/server.js
@@ -1,12 +1,15 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const connectDB = require("../Backend/Database/Connection");
+const userRoutes = require('../Backend/Routes/UserRoutes');
+const historyRoutes = require('../Backend/Routes/HistoryRoutes')
 dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 8000;
 app.use(express.json());
 connectDB();
-
+app.use('/api/users',userRoutes)
+app.use('api/history',historyRoutes)
 app.listen(PORT,()=>{
     console.log(`Server is running on port ${PORT}`);
 })
