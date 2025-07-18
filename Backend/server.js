7842a9fa180cfa2f4154dcc99e56707f6ff0ecb2
vishalsuriya <vishalsuriya2003@gmail.com>
2025-07-18 08:43:47 +0530

    `Added MONGO_URI to .env , connected the database to the server and created schemas

--- Backend/server.js
+++ Backend/server.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const connectDB = require("../Backend/Database/Connection");
 dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 8000;
 app.use(express.json());
-
+connectDB();
 
 app.listen(PORT,()=>{
     console.log(`Server is running on port ${PORT}`);
